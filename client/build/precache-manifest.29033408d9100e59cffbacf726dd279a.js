cc7df29a5c69ddab0d8d28e536243ac32d69dcee
Dalina Weidinger <dalina.weidinger@gmail.com>
2020-03-22 19:17:00 +0100

    herkou deploy

---
+++ client/build/precache-manifest.29033408d9100e59cffbacf726dd279a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c15e53c954eaefa146f081cea76d0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c064beff9dd8d3fbfc5f",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "3de56e09307b974ebb60",
+    "url": "/static/css/main.e77c9371.chunk.css"
+  },
+  {
+    "revision": "c064beff9dd8d3fbfc5f",
+    "url": "/static/js/2.26e33892.chunk.js"
+  },
+  {
+    "revision": "3de56e09307b974ebb60",
+    "url": "/static/js/main.1da2d16c.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  },
+  {
+    "revision": "3ad27c1ec65e7a791e484488ed78615a",
+    "url": "/static/media/add.3ad27c1e.png"
+  },
+  {
+    "revision": "a9945377bda3cba8507fa1e4a094d6f7",
+    "url": "/static/media/chat.a9945377.png"
+  },
+  {
+    "revision": "0f85f4293f38a435def4900ef6278e12",
+    "url": "/static/media/gift.0f85f429.png"
+  },
+  {
+    "revision": "3a636e9d2cf28fb01998ad4076fc7e09",
+    "url": "/static/media/heart.3a636e9d.png"
+  },
+  {
+    "revision": "20e7d5361204b4ec2d93f0627d3b836a",
+    "url": "/static/media/star.20e7d536.png"
+  }
+]);
