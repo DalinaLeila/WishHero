8bbed1849fdb52cf17a8d05634a1358eb7b78240
Dalina Weidinger <dalina.weidinger@gmail.com>
2020-03-23 20:31:05 +0100

    landing page

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -10,7 +10,7 @@ const Landing = () => {
           <h1>Platform to create and share wishlists</h1>
           <p>
             WishHero lets you keep track of your wishlists, fullfill other users
-            wishes and communicate with other WishHero users{" "}
+            wishes and communicate with other WishHero members{" "}
           </p>
           <Link to="/signup">
             {" "}
@@ -20,6 +20,30 @@ const Landing = () => {
       </div>
       <div className="white">
         <img width="900px" src={require("./assets/responsive.png")} alt="" />
+        <div className="features">
+          <h1>Create. Share. Explore.</h1>
+          <ul>
+            <li>Create as many wish lists as you like</li>
+            <li>Easily add gifts, include links and upload pictures</li>
+            <li>
+              Explore other users wishlists and make their dreams come true
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="phone-right">
+        <h1>Easy to manage</h1>
+        <img
+          className="phone-overlay"
+          width="300px"
+          src={require("./assets/chat_phone.png")}
+          alt=""
+        />
+        <img width="300px" src={require("./assets/profile_phone.png")} alt="" />
+        <h1>Easy to add</h1>
+      </div>
+      <div className="footer">
+        &copy; {new Date().getFullYear()} Dalina Weidinger
       </div>
     </>
   );
