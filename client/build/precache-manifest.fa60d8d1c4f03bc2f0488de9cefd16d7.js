7513facffd0335852311236300bb384c02604d6f
Dalina Weidinger <dalina.weidinger@gmail.com>
2020-03-23 20:49:15 +0100

    fix heroku

---
+++ client/build/precache-manifest.fa60d8d1c4f03bc2f0488de9cefd16d7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3710e266b73294969352d042339242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f692539f2389f8d31e4",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4bae65bbe78b424562d0",
+    "url": "/static/css/main.36eb1bc7.chunk.css"
+  },
+  {
+    "revision": "4f692539f2389f8d31e4",
+    "url": "/static/js/2.6e4f2081.chunk.js"
+  },
+  {
+    "revision": "4bae65bbe78b424562d0",
+    "url": "/static/js/main.46f28a47.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  },
+  {
+    "revision": "3ad27c1ec65e7a791e484488ed78615a",
+    "url": "/static/media/add.3ad27c1e.png"
+  },
+  {
+    "revision": "a9945377bda3cba8507fa1e4a094d6f7",
+    "url": "/static/media/chat.a9945377.png"
+  },
+  {
+    "revision": "a139f62fb4af46722cc5929e1da57c27",
+    "url": "/static/media/chat_phone.a139f62f.png"
+  },
+  {
+    "revision": "0f85f4293f38a435def4900ef6278e12",
+    "url": "/static/media/gift.0f85f429.png"
+  },
+  {
+    "revision": "3a636e9d2cf28fb01998ad4076fc7e09",
+    "url": "/static/media/heart.3a636e9d.png"
+  },
+  {
+    "revision": "3544125fb4e704b315bc73de7db03639",
+    "url": "/static/media/profile_phone.3544125f.png"
+  },
+  {
+    "revision": "361412a5f2f04a23804cf522ad1ac31d",
+    "url": "/static/media/responsive.361412a5.png"
+  },
+  {
+    "revision": "20e7d5361204b4ec2d93f0627d3b836a",
+    "url": "/static/media/star.20e7d536.png"
+  }
+]);
